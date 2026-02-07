70053bbc3ca0ec91f827c6d67bdb1eb80c71d0bb
saislam-sigmanticai <sulaiman@sigmanticai.com>
2026-02-06 23:08:29 -0800

    Update init.py for merger

--- step5_coverage/metrics/init.py
+++ step5_coverage/metrics/init.py
@@ -13,10 +13,21 @@ from .calculator import (
     calculate_coverage,
     quick_summary,
 )
+from .merger import (
+    CoverageMerger,
+    MergeStatistics,
+    merge_coverage_simple,
+    merge_coverage_with_tracking,
+)
 
 __all__ = [
     "CoverageCalculator",
     "UncoveredRegion",
     "calculate_coverage",
     "quick_summary",
+    # Merger
+    "CoverageMerger",
+    "MergeStatistics",
+    "merge_coverage_simple",
+    "merge_coverage_with_tracking",
 ]
